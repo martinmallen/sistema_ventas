d6b0296fee2f97e56dd930afc43589327a091282
martinmallen <martinmallen@live.cl>
2017-11-16 12:09:14 -0300

    actualizar commit yhaciendo ctrl

---
+++ src/main/Java/cl/accenture/curso_java/sistema_ventas/controladores/UsuarioControlador.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package cl.accenture.curso_java.sistema_ventas.controladores;
+
+import java.io.Serializable;
+
+/**
+ * @author Martin Cuevas
+ *
+ */
+public class UsuarioControlador implements Serializable {
+
+}
