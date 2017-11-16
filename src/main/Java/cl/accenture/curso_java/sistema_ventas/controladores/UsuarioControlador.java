2a2144f42d70028b9be7992086da703c6b5928e9
martinmallen <martinmallen@live.cl>
2017-11-16 12:53:02 -0300

    kak

--- src/main/Java/cl/accenture/curso_java/sistema_ventas/controladores/UsuarioControlador.java
+++ src/main/Java/cl/accenture/curso_java/sistema_ventas/controladores/UsuarioControlador.java
@@ -4,6 +4,7 @@
 package cl.accenture.curso_java.sistema_ventas.controladores;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @author Martin Cuevas
@@ -15,5 +16,6 @@ public class UsuarioControlador implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 6033183801879955808L;
-
+	private String rut, nombre, password, email;
+	
 }
