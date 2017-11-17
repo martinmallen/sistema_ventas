3c0c388644a082c9ebef678cd98878c390bf9ff4
martinmallen <martinmallen@live.cl>
2017-11-17 19:32:46 -0300

    vistas martin y maxi agregadas y ancladas, sucursalCtrl y DAO, fondo4.jpg

---
+++ src/main/Java/cl/accenture/curso_java/sistema_ventas/dao/SucursalDAO.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package cl.accenture.curso_java.sistema_ventas.dao;
+
+
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import cl.accenture.curso_java.sistema_ventas.excepciones.SinConexionException;
+import cl.accenture.curso_java.sistema_ventas.modelo.Conexion;
+import cl.accenture.curso_java.sistema_ventas.modelo.Sucursal;
+
+/**
+ * @author Martin Cuevas
+ *
+ */
+public class SucursalDAO {
+	private Conexion conexion;
+
+	/**
+	 * 
+	 */
+	public SucursalDAO() {
+		this.conexion = new Conexion();
+	}
+
+	/**
+	 * @param conexion
+	 */
+	public SucursalDAO(Conexion conexion) {
+		this.conexion = conexion;
+	}
+
+	/**
+	 * @return the conexion
+	 */
+	public Conexion getConexion() {
+		return conexion;
+	}
+
+	/**
+	 * @param conexion
+	 *            the conexion to set
+	 */
+	public void setConexion(Conexion conexion) {
+		this.conexion = conexion;
+	}
+	
+	public void insertarSucursal(Sucursal sucursal) throws SQLException, SinConexionException {
+		PreparedStatement psInsert = conexion.obtenerConexion().prepareStatement("INSERT INTO Sucursal(idSucursal,nombre,direccion)" 
+				+ "VALUES (?, ?, ?);");
+		psInsert.setInt(1, sucursal.getIdSucursal());
+		psInsert.setString(2, sucursal.getNombre());
+		psInsert.setString(3, sucursal.getDireccion());
+		psInsert.executeUpdate();
+	}
+	
+}
