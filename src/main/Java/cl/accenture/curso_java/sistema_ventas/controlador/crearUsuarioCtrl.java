701f60f5143b56eb1c9887840ccf962f9f1760de
martinmallen <martinmallen@live.cl>
2017-11-20 12:21:51 -0300

    push de prueba

--- src/main/Java/cl/accenture/curso_java/sistema_ventas/controlador/crearUsuarioCtrl.java
+++ src/main/Java/cl/accenture/curso_java/sistema_ventas/controlador/crearUsuarioCtrl.java
@@ -4,31 +4,22 @@
 package cl.accenture.curso_java.sistema_ventas.controlador;
 
 import java.io.Serializable;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-
-import cl.accenture.curso_java.sistema_ventas.dao.SucursalDAO;
 import cl.accenture.curso_java.sistema_ventas.dao.UsuarioDAO;
 import cl.accenture.curso_java.sistema_ventas.excepciones.SinConexionException;
-import cl.accenture.curso_java.sistema_ventas.modelo.Conexion;
 import cl.accenture.curso_java.sistema_ventas.modelo.Usuario;
 
 /**
  * @author Martin Cuevas
  *
  */
-@ManagedBean
-@SessionScoped
-public class LoginCtrl implements Serializable {
+public class crearUsuarioCtrl implements Serializable {
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -8167310269289072882L;
+	private static final long serialVersionUID = -2570733654212148470L;
 	private String nombre;
 	private String apellido;
 	private String password;
@@ -38,12 +29,10 @@ public class LoginCtrl implements Serializable {
 	private String perfil_nombre;
 	private int idSucursal;
 	private String mensaje;
-	private Conexion conexion;
-
 	/**
 	 * 
 	 */
-	public LoginCtrl() {
+	public crearUsuarioCtrl() {
 		this.nombre = "";
 		this.apellido = "";
 		this.password = "";
@@ -53,9 +42,7 @@ public class LoginCtrl implements Serializable {
 		this.perfil_nombre = "";
 		this.idSucursal = 0;
 		this.mensaje = "";
-		this.conexion = new Conexion();
 	}
-
 	/**
 	 * @param nombre
 	 * @param apellido
@@ -64,11 +51,11 @@ public class LoginCtrl implements Serializable {
 	 * @param rut
 	 * @param estado
 	 * @param perfil_nombre
-	 * @param sucursal_idSucursal
-	 * @param conexion
+	 * @param idSucursal
+	 * @param mensaje
 	 */
-	public LoginCtrl(String nombre, String apellido, String password, String email, String rut, boolean estado,
-			String perfil_nombre, int sucursal_idSucursal, String mensaje, Conexion conexion) {
+	public crearUsuarioCtrl(String nombre, String apellido, String password, String email, String rut, boolean estado,
+			String perfil_nombre, int idSucursal, String mensaje) {
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.password = password;
@@ -76,177 +63,125 @@ public class LoginCtrl implements Serializable {
 		this.rut = rut;
 		this.estado = estado;
 		this.perfil_nombre = perfil_nombre;
-		this.idSucursal = sucursal_idSucursal;
+		this.idSucursal = idSucursal;
 		this.mensaje = mensaje;
-		this.conexion = conexion;
 	}
-
 	/**
 	 * @return the nombre
 	 */
 	public String getNombre() {
 		return nombre;
 	}
-
 	/**
-	 * @param nombre
-	 *            the nombre to set
+	 * @param nombre the nombre to set
 	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 	/**
 	 * @return the apellido
 	 */
 	public String getApellido() {
 		return apellido;
 	}
-
 	/**
-	 * @param apellido
-	 *            the apellido to set
+	 * @param apellido the apellido to set
 	 */
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-
 	/**
 	 * @return the password
 	 */
 	public String getPassword() {
 		return password;
 	}
-
 	/**
-	 * @param password
-	 *            the password to set
+	 * @param password the password to set
 	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	/**
 	 * @return the email
 	 */
 	public String getEmail() {
 		return email;
 	}
-
 	/**
-	 * @param email
-	 *            the email to set
+	 * @param email the email to set
 	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	/**
 	 * @return the rut
 	 */
 	public String getRut() {
 		return rut;
 	}
-
 	/**
-	 * @param rut
-	 *            the rut to set
+	 * @param rut the rut to set
 	 */
 	public void setRut(String rut) {
 		this.rut = rut;
 	}
-
 	/**
 	 * @return the estado
 	 */
 	public boolean isEstado() {
 		return estado;
 	}
-
 	/**
-	 * @param estado
-	 *            the estado to set
+	 * @param estado the estado to set
 	 */
 	public void setEstado(boolean estado) {
 		this.estado = estado;
 	}
-
 	/**
 	 * @return the perfil_nombre
 	 */
 	public String getPerfil_nombre() {
 		return perfil_nombre;
 	}
-
 	/**
-	 * @param perfil_nombre
-	 *            the perfil_nombre to set
+	 * @param perfil_nombre the perfil_nombre to set
 	 */
 	public void setPerfil_nombre(String perfil_nombre) {
 		this.perfil_nombre = perfil_nombre;
 	}
-
 	/**
-	 * @return the sucursal_idSucursal
+	 * @return the idSucursal
 	 */
-	public int getidSucursal() {
+	public int getIdSucursal() {
 		return idSucursal;
 	}
-
 	/**
-	 * @param sucursal_idSucursal
-	 *            the sucursal_idSucursal to set
+	 * @param idSucursal the idSucursal to set
 	 */
-	public void setidSucursal(int sucursal_idSucursal) {
-		idSucursal = sucursal_idSucursal;
+	public void setIdSucursal(int idSucursal) {
+		this.idSucursal = idSucursal;
 	}
-
 	/**
 	 * @return the mensaje
 	 */
 	public String getMensaje() {
 		return mensaje;
 	}
-
 	/**
-	 * @param mensaje
-	 *            the mensaje to set
+	 * @param mensaje the mensaje to set
 	 */
 	public void setMensaje(String mensaje) {
 		this.mensaje = mensaje;
 	}
+	
+	public void guardarUsuario() {
+		Usuario usuario = new Usuario(this.rut, this.nombre, this.password, this.email, this.perfil_nombre,
+				this.apellido, this.idSucursal);
+		UsuarioDAO dao = new UsuarioDAO();
 
-	/**
-	 * @return the conexion
-	 */
-	public Conexion getConexion() {
-		return conexion;
-	}
-
-	/**
-	 * @param conexion
-	 *            the conexion to set
-	 */
-	public void setConexion(Conexion conexion) {
-		this.conexion = conexion;
-	}
-
-	public String ingresar() {
 		try {
-			PreparedStatement psSelect = conexion.obtenerConexion()
-					.prepareStatement("SELECT * FROM usuario Where rut = ?;");
-			psSelect.setString(1, this.rut);
-			ResultSet rs = psSelect.executeQuery();
-			while (rs.next()) {
-				if (rs.getString("password").equals(this.password)) {
-					this.nombre = rs.getString("nombre");
-					this.apellido = rs.getString("apellido");
-					this.email = rs.getString("email");
-					this.perfil_nombre = rs.getString("perfil_nombre");
-					this.idSucursal = rs.getInt("Sucursal_idSucursal");
-					return "principal.xhtml";
-				}
-			}
+			dao.ingresarUsuario(usuario);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -254,23 +189,6 @@ public class LoginCtrl implements Serializable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		this.mensaje = "ACCESO DENEGADO, rut o password Incorrectos.";
-		return mensaje;
 	}
 
-	public String cerrarSesion() {
-		this.nombre = "";
-		this.apellido = "";
-		this.password = "";
-		this.email = "";
-		this.rut = "";
-		this.estado = true;
-		this.perfil_nombre = "";
-		this.idSucursal = 0;
-		this.mensaje = "";
-		this.conexion = new Conexion();
-		return "login_.xhtml";
-	}
-
-	
 }
