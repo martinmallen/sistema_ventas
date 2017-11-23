d0030a83deedf13e624d917c9c8b0bc31b8d4015
martinmallen <martinmallen@live.cl>
2017-11-23 12:14:23 -0300

    att final

--- src/main/Java/cl/accenture/curso_java/sistema_ventas/controlador/ListarProductosControlador.java
+++ src/main/Java/cl/accenture/curso_java/sistema_ventas/controlador/ListarProductosControlador.java
@@ -1,7 +1,6 @@
 package cl.accenture.curso_java.sistema_ventas.controlador;
 
 import java.io.Serializable;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -14,10 +13,7 @@ import javax.faces.context.FacesContext;
 import org.apache.log4j.Logger;
 
 import cl.accenture.curso_java.sistema_ventas.dao.ProductoDAO;
-import cl.accenture.curso_java.sistema_ventas.dao.SucursalDAO;
-import cl.accenture.curso_java.sistema_ventas.excepciones.SinConexionException;
 import cl.accenture.curso_java.sistema_ventas.modelo.Producto;
-import cl.accenture.curso_java.sistema_ventas.modelo.Sucursal;
 import cl.accenture.curso_java.sistema_ventas.modelo.Usuario;
 
 /**
@@ -47,7 +43,9 @@ public class ListarProductosControlador implements Serializable {
 	 * @return the productos
 	 */
 	public List<Producto> getProductos() {
-	
+		
+		return productos;
+	}
 
 
 
@@ -98,6 +96,20 @@ public class ListarProductosControlador implements Serializable {
 
 
 
+	public void obtenerProductos() {
+
+		try {
+			ProductoDAO dao = new ProductoDAO();
+			this.setProductos(dao.obtenerProductos());
+			this.setMensaje("");
+
+		} catch (Exception e) {
+			this.setMensaje("Ocurrio un error al obtener los productos.");
+			this.setProductos(new ArrayList<Producto>());
+			LOGGER.error("Error al obtener los productos", e);
+		}
+
+	}
 
 	public void obtenerProductosSucursal() {
 
@@ -107,18 +119,12 @@ public class ListarProductosControlador implements Serializable {
 			ProductoDAO dao = new ProductoDAO();
 			this.setProductosSucursal(dao.obtenerProductosSucursal(user.getIdSucursal()));
 
-		public List<Producto> getProductos() {
-			return productos;
-
-			
-
 			this.setMensaje("");
 			
 		} catch (Exception e) {
 			this.setMensaje("Ocurrio un error al obtener los productos.");
 			this.setProductosSucursal(new ArrayList<Producto>());
 			LOGGER.error("Error al obtener los productos por sucursal", e);
-
 		}
 		
 	}
@@ -133,6 +139,9 @@ public class ListarProductosControlador implements Serializable {
 					return -1;
 				return 0;
 			}
+
+		});
+	}
 
 	public void ordenarPorNombre() {
 		Collections.sort(this.productos, new Comparator<Producto>() {
@@ -167,25 +176,12 @@ public class ListarProductosControlador implements Serializable {
 	public void ordenarPorPrecio() {
 		Collections.sort(this.productos, new Comparator<Producto>() {
 
-
-		public void obtenerProductosSucursal(){
-			List<Producto> productos = new ArrayList<Producto>();
-			
-			try{
-				ProductoDAO dao = new ProductoDAO();
-			productos =	dao.obtenerProductosSucursal(this.idSucursal);
-				this.productos = productos;
-				this.setMensaje("");
-			}catch (Exception e) {
-				this.setMensaje("Ocurrio un error al obtener los productos.");
-
 			public int compare(Producto p1, Producto p2) {
 				if (p1.getPrecio() > p2.getPrecio())
 					return 1;
 				if (p1.getPrecio() < p2.getPrecio())
 					return -1;
 				return 0;
-
 			}
 
 		});
