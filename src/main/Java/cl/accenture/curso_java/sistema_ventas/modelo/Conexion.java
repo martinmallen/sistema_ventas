ee5a8c712814fe34f205262e6ec3eb10a779af7b
mauriormu <mauricio.ortega@usach.cl>
2017-11-29 10:13:47 -0300

    vistas

--- src/main/Java/cl/accenture/curso_java/sistema_ventas/modelo/Conexion.java
+++ src/main/Java/cl/accenture/curso_java/sistema_ventas/modelo/Conexion.java
@@ -22,12 +22,8 @@ public class Conexion {
 	public Conexion() {
 		this.host = "127.0.0.1"; // o "localhost"
 		this.baseDeDatos = "sisinventario";
-<<<<<<< HEAD
 		this.nombre = "root";
-=======
-		this.nombre = "alumnos";
->>>>>>> 443a7835f58cadd5b28fa4ff64c621a502209469
-		this.password = "java123";
+		this.password = "wasd231";
 		this.puerto = 3306;
 	}
 
