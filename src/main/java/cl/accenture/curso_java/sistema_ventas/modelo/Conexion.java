73271cb52c1d55e55a7ca5124845234340a9a792
martinmallen <martinmallen@live.cl>
2017-12-03 16:25:01 -0300

    att

--- src/main/java/cl/accenture/curso_java/sistema_ventas/modelo/Conexion.java
+++ src/main/java/cl/accenture/curso_java/sistema_ventas/modelo/Conexion.java
@@ -25,7 +25,7 @@ public class Conexion {
 		this.host = "127.0.0.1"; // o "localhost"
 		this.baseDeDatos = "sisinventario";
 		this.nombre = "root";
-		this.password = "java123";
+		this.password = "m4l3np1wk3";
 		this.puerto = 3306;
 		
 		Map<String, String> env = System.getenv();
